9f5e3853d145e03781acb3cbef060ff21435e583
Jean Latiere <jean@optimnow.io>
2025-12-12 15:45:11 +0100

    Update vite.config.ts
    for openAI key

--- vite.config.ts
+++ vite.config.ts
@@ -10,10 +10,10 @@ export default defineConfig(({ mode }) => {
         host: '0.0.0.0',
       },
       plugins: [react()],
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
+        define: {
+          'process.env.API_KEY': JSON.stringify(env.OPENAI_API_KEY),
+          'process.env.OPENAI_API_KEY': JSON.stringify(env.OPENAI_API_KEY)
+        },
       resolve: {
         alias: {
           '@': path.resolve(__dirname, '.'),
